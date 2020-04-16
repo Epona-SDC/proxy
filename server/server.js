7ca6a78e6b85801b2fd1b9bbb7b1605e78b44a8b
Ashby Gunter <theshbee@yahoo.com>
2020-04-16 13:51:38 -0700

    Updates for use on deployed service

--- server/server.js
+++ server/server.js
@@ -3,15 +3,19 @@ const cors = require('cors');
 const request = require('request');
 const port = 3000;
 
+// const servicePath = 'http://localhost:3003';
+const servicePath = 'http://52.15.41.113';
+
+
 let app = express();
 
 // app.use(express.json());
 app.use(express.static(__dirname + '/../public'));
 
 
-app.post("/api/rentals", (req, res) => {
+app.post(`/api/rentals`, (req, res) => {
   // console.log('POST request received, trying to pass to service');
-  const passAlongUrl = 'http://localhost:3003/api/rentals';
+  const passAlongUrl = `${servicePath}/api/rentals`;
   req.pipe(request(passAlongUrl)).pipe(res);
 });
 
