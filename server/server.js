6cfd1600119d4dbfb9f2351210bc8dd99a7497c2
Ashby Gunter <theshbee@yahoo.com>
2020-04-17 22:42:09 -0700

    Changes to function deployed
    Includes updating IP for script in index.html, IP for the service
    and building a GET API for api/rentals to go to service through proxy

--- server/server.js
+++ server/server.js
@@ -2,26 +2,27 @@ require('newrelic');
 
 const express = require('express');
 const cors = require('cors');
+// const morgan = require("morgan");
 const request = require('request');
 const port = 3000;
 
 // const servicePath = 'http://localhost:3003';
-const servicePath = 'http://52.15.41.113';
-
+const bookingsPath = 'http://52.15.41.113:3003';
+const bookingsAPI = `${bookingsPath}/api/rentals`;
 
 let app = express();
 
-// app.use(express.json());
+// app.use(morgan('dev'));
+
 app.use(express.static(__dirname + '/../public'));
 
+app.get(`/api/rentals`, (req, res) => {
+  const id = parseInt(req.query.id);
+  const query = `${bookingsAPI}?id=${id}`
+  req.pipe(request(query)).pipe(res);
+});
 
 app.post(`/api/rentals`, (req, res) => {
   // console.log('POST request received, trying to pass to service');
-  const passAlongUrl = `${servicePath}/api/rentals`;
-  req.pipe(request(passAlongUrl)).pipe(res);
-});
-
-
-app.listen(port, err => {
-  console.log(`Listening on port ${port}...`);
+  req.pipe(request(bookingsAPI)).pipe(res);
 });
