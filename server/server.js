19fb6b42717919c9fe2f865e7af369b971ea83b8
Ashby Gunter <theshbee@yahoo.com>
2020-04-23 12:31:19 -0700

    Updates for deployment

--- server/server.js
+++ server/server.js
@@ -7,18 +7,31 @@ const request = require('request');
 const port = 3000;
 
 // const servicePath = 'http://localhost:3003';
-const bookingsPath = 'http://52.15.41.113:3003';
-const bookingsAPI = `${bookingsPath}/api/rentals`;
+const bookingsPath1 = 'http://52.15.41.113:3003';
+const bookingsPath2 = 'http://52.15.92.56:3003';
+const bookingsAPI = `/api/rentals`;
+
+const paths = [bookingsPath1, bookingsPath2];
+let path = 0;
 
 let app = express();
 
 // app.use(morgan('dev'));
 
+const toggle = function() {
+  if (path === 0) {
+    path = 1;
+  } else {
+    path = 0;
+  }
+}
+
 app.use(express.static(__dirname + '/../public'));
 
 app.get(`/api/rentals`, (req, res) => {
   const id = parseInt(req.query.id);
-  const query = `${bookingsAPI}?id=${id}`
+  toggle();
+  const query = `${paths[path]}${bookingsAPI}?id=${id}`
   req.pipe(request(query)).pipe(res);
 });
 
